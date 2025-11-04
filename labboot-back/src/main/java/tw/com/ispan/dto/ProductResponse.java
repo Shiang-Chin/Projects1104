29719db0db59cc204b2b3bf5899f92c3472bb77a
Shiang Chin <s50006147@gmail.com>
2025-11-04 16:18:04 +0800

    新增移除檔案

--- labboot-back/src/main/java/tw/com/ispan/dto/ProductResponse.java
+++
@@ -1,9 +0,0 @@
-package tw.com.ispan.dto;
-
-import java.util.List;
-
-import tw.com.ispan.domain.ProductBean;
-
-public record ProductResponse(String message, Boolean success, Long count, List<ProductBean> list) {
-
-}
